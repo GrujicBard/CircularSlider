62858beacabed78d10736132072d91d7fbfb60c2
GrujicBard <33715866+GrujicBard@users.noreply.github.com>
2023-10-02 21:36:52 +0200

    Made createCricle and drawPoint dynamic

--- app.js
+++ app.js
@@ -12,39 +12,38 @@ class Slider {
     draw() {
         this.createLegend();
         this.createCanvas();
+        this.sliders.forEach(slider => {
+            this.drawCircle(slider.radius);
+            this.drawPoint(slider.radius, 0);
+        });
+
     }
 
     createCanvas() {
-        const div = document.createElement("div");
+        let div = document.createElement("div");
         div.classList.add("canvas_container");
-        const canvas = document.createElement("canvas");
-        canvas.setAttribute("id","slider_canvas");
+        let canvas = document.createElement("canvas");
+        canvas.setAttribute("id", "slider_canvas");
         canvas.width = this.height;
         canvas.height = this.width;
         div.appendChild(canvas);
         this.container.appendChild(div);
-
-        this.drawCircle(canvas);
-        this.drawPoint(canvas, 0);
-        this.drawPoint(canvas, 90);
-        this.drawPoint(canvas, 180);
-        this.drawPoint(canvas, 270);
     }
 
-    drawCircle(canvas) {
-        const ctx = canvas.getContext("2d");
-        this.sliders.forEach(slider => {
-            ctx.beginPath();
-            ctx.arc(this.height/2, this.width/2,slider.radius,0,2*Math.PI);
-            ctx.stroke();
-        });
+    drawCircle(radius) {
+        let canvas = document.getElementById("slider_canvas");
+        let ctx = canvas.getContext("2d");
+        ctx.beginPath();
+        ctx.arc(this.height / 2, this.width / 2, radius, 0, 2 * Math.PI);
+        ctx.stroke();
     }
 
-    drawPoint(canvas, angle){
-        const ctx = canvas.getContext("2d");
-        var x = this.width/2 + 200 * Math.cos(-angle*Math.PI/180);
-        var y = this.height/2 + 200 * Math.sin(-angle*Math.PI/180);
-    
+    drawPoint(radius, angle) {
+        let canvas = document.getElementById("slider_canvas");
+        let ctx = canvas.getContext("2d");
+        let x = this.width / 2 + radius * Math.cos(-angle * Math.PI / 180);
+        let y = this.height / 2 + radius * Math.sin(-angle * Math.PI / 180);
+
         ctx.beginPath();
         ctx.arc(x, y, this.point_size, 0, 2 * Math.PI);
         ctx.fill();
@@ -53,30 +52,30 @@ class Slider {
     createLegend() {
 
         // Legend div
-        const div = document.createElement("div");
+        let div = document.createElement("div");
         div.classList.add("legend_container");
         // Table
-        const table = document.createElement("table");
+        let table = document.createElement("table");
         table.classList.add("table");
         // Data for each slider
         this.sliders.forEach((slider, index) => {
 
-            const tr = document.createElement("tr");
+            let tr = document.createElement("tr");
             // Value
-            const td_1 = document.createElement("td");
+            let td_1 = document.createElement("td");
             td_1.setAttribute("value", index);
             td_1.innerText = slider.initialValue ?? 0;
 
             // Color
-            const td_2 = document.createElement("td");
-            const colorBox = document.createElement("span");
+            let td_2 = document.createElement("td");
+            let colorBox = document.createElement("span");
             colorBox.style.backgroundColor = slider.color ?? "#000000";
             colorBox.classList.add('color_box');
             colorBox.innerHTML = "&nbsp";
             td_2.appendChild(colorBox);
 
             // Name
-            const td_3 = document.createElement("td");
+            let td_3 = document.createElement("td");
             td_3.setAttribute("name", index);
             td_3.innerText = slider.name ?? "No name";
 
