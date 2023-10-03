7a935d8b6d22dab3062be098c24888dfeff2908d
GrujicBard <33715866+GrujicBard@users.noreply.github.com>
2023-10-03 12:37:17 +0200

    Changed naming of properties

--- app.js
+++ app.js
@@ -1,44 +1,42 @@
 class Slider {
 
-    constructor({ container, sliderOptions }) {
+    constructor({ container, slider_options }) {
         this.container = document.querySelector(container);
-        this.sliderOptions = sliderOptions
+        this.slider_options = slider_options
 
-        this.svgHeight = 500;
-        this.svgWidth = 500;
-        this.x = this.svgHeight / 2;
-        this.y = this.svgWidth / 2;
+        this.svg_height = 500;
+        this.svg_width = 500;
+        this.x = this.svg_height / 2;
+        this.y = this.svg_width / 2;
         this.ns = "http://www.w3.org/2000/svg";
         this.point_size = 14;   // Size of the slider point
 
-        this.tempSliderOption = this.sliderOptions[0];
+        this.tempSliderOption = this.slider_options[0];
     }
 
     draw() {
         this.createLegend();
 
-        let svgContainer = document.createElement("div");
-        svgContainer.classList.add("svg_container");
-        svgContainer.setAttribute("data-svg-holder", true);
+        let svg_container = document.createElement("div");
+        svg_container.classList.add("svg_container");
+        svg_container.setAttribute("data-svg-holder", true);
         let svg_holder = document.createElementNS(this.ns, "svg");
-        svg_holder.setAttribute("width", this.svgWidth);
-        svg_holder.setAttribute("height", this.svgWidth);
-        svgContainer.appendChild(svg_holder);
-        this.container.appendChild(svgContainer);
-        this.sliderOptions.forEach((sliderOpt, index) => {
-            this.drawSlider(sliderOpt, index, svg_holder);
+        svg_holder.setAttribute("width", this.svg_width);
+        svg_holder.setAttribute("height", this.svg_width);
+        svg_container.appendChild(svg_holder);
+        this.container.appendChild(svg_container);
+        this.slider_options.forEach((slider_opt, index) => {
+            this.drawSlider(slider_opt, index, svg_holder);
         });
-
-
     }
 
-    drawSlider(sliderOpt, index, svg_holder){
+    drawSlider(slider_opt, index, svg_holder){
         let slider = document.createElementNS(this.ns, "g");
         slider.setAttribute("data-slider", index);
         svg_holder.appendChild(slider);
         svg_holder.appendChild(slider);
-        this.drawCircle(sliderOpt.radius, index, slider);
-        this.drawPoint(sliderOpt.radius, sliderOpt.initialValue, sliderOpt.color, index, slider);
+        this.drawCircle(slider_opt.radius, index, slider);
+        this.drawPoint(slider_opt.radius, slider_opt.min, slider_opt.max, slider_opt.initial_value, slider_opt.color, index, slider);
     }
 
     drawCircle(radius, index, svg) {
@@ -52,12 +50,12 @@ class Slider {
         svg.appendChild(circle);
     }
 
-    drawPoint(radius, initialValue, color, index, svg) {
-        let initialAngle = 360 * initialValue / (this.tempSliderOption.max - this.tempSliderOption.min);
+    drawPoint(radius, min, max, initial_value, color, index, svg) {
+        let initial_angle = 360 * initial_value / (max - min);
         let point = document.createElementNS(this.ns, "circle");
         point.setAttribute("data-point", index);
-        let x = this.x + radius * Math.cos((initialAngle - 90) * Math.PI / 180);
-        let y = this.y + radius * Math.sin((initialAngle - 90) * Math.PI / 180);
+        let x = this.x + radius * Math.cos((initial_angle - 90) * Math.PI / 180);
+        let y = this.y + radius * Math.sin((initial_angle - 90) * Math.PI / 180);
         point.setAttribute("cx", x);
         point.setAttribute("cy", y);
         point.setAttribute("r", this.point_size);
@@ -74,7 +72,7 @@ class Slider {
         let table = document.createElement("table");
         table.classList.add("table");
         // Data for each slider
-        this.sliderOptions.forEach((slider, index) => {
+        this.slider_options.forEach((slider, index) => {
 
             let tr = document.createElement("tr");
             // Value
